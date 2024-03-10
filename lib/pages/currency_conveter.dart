225f6eff4e40fbfdf911b7bb578fc3cda1b456b3
mhbabon <mhbabon.bd@gmail.com>
2024-03-10 12:03:20 +0600

    Its a currency conveter app which is convert USD to BDT

---
+++ lib/pages/currency_conveter.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+   double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+   void convert(){
+     setState(() {
+       result = double.parse(textEditingController.text)*103;
+     });
+   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title:const Text(
+          'Currency Converter',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Container(
+          width: double.maxFinite,
+          height: double.maxFinite,
+          decoration: const BoxDecoration(color: Colors.greenAccent),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                 Text(
+                  result.toString(),
+                  style:const TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextField(
+                  controller: textEditingController,
+                  keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true,
+                  ),
+                  style: const TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                      hintText: 'Please Enter the amount of USD',
+                      hintStyle: const TextStyle(color: Colors.black45),
+                      fillColor: Colors.white,
+                      filled: true,
+                      prefixIcon: const Icon(Icons.monetization_on_outlined),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(width: 2),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(20),
+                      )),
+                ),
+                const SizedBox(height: 50),
+                ElevatedButton(
+                    onPressed: convert,
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.white,
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: const Text(
+                      'CONVERT',
+                    ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
